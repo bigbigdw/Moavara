6a73b9aa88510897d53c2b8762e31cbab1c7e39d
bigbigdw <kdw0310@ajou.ac.kr>
2021-12-22 00:39:05 +0900

    2021.12.22 베스트쪽 작업중

---
+++ app/src/main/java/com/example/takealook/Best/FragmentBest.kt
@@ -0,0 +1,24 @@
+package com.example.takealook.Best
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.takealook.R
+
+
+class FragmentBest : Fragment() {
+
+    lateinit var root: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        root = inflater.inflate(R.layout.fragment_best, container, false)
+
+        return root
+    }
+
+}
