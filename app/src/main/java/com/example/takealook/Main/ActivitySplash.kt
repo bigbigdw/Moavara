41e52ae85683675500f0d52ad2d24ec6497d764f
dwkim <kdw0310@ajou.ac.kr>
2022-01-27 18:47:07 +0900

    2022.01.27 원스토어 작업중

--- app/src/main/java/com/example/takealook/Main/ActivitySplash.kt
+++ app/src/main/java/com/example/takealook/Main/ActivitySplash.kt
@@ -47,7 +47,8 @@ class ActivitySplash : Activity() {
 
     private fun test() {
 
-        getRidiBest()
+//        getRidiBest()
+        getOneStoreBest()
 
     }
 
@@ -89,6 +90,52 @@ class ActivitySplash : Activity() {
                         Calendar.getInstance().get(Calendar.MONTH),
                         10 - i,
                         "Ridi"
+                    )
+                )
+            }
+        }
+
+    }
+
+    private fun getOneStoreBest() {
+
+        val doc: Document =
+            Jsoup.connect("https://onestory.co.kr/display/card/CRD0059596?title=%EC%9D%B4%EA%B1%B4%20%EC%82%AC%EC%95%BC%ED%95%B4!%20%EC%9C%A0%EB%A3%8C%20%EA%B5%AC%EB%A7%A4%EC%9C%A8%20BEST%20%EB%A1%9C%EB%A7%A8%EC%8A%A4").get()
+        val ridiKeyword: Elements = doc.select(".ItemRendererInner")
+
+        for (i in ridiKeyword.indices) {
+            if (i < 20) {
+
+                Log.d("!!!!", doc.select(".tItemRendererTextPublisher")[i].text())
+
+                val writerName = doc.select("div .ItemRendererTextArtist")[i].text()
+                val subject = doc.select("div .ItemRendererTextTitle")[i].text()
+                val bookImg = doc.select(".ThumbnailInner img")[i].absUrl("src")
+                val intro = doc.select(".tItemRendererTextPublisher")[i].text()
+                val bookCode = ""
+                val cntChapter = doc.select(".tItemRendererTextSeries")[i].text()
+                val cntPageRead = doc.select(".ItemRendererTextComment span span")[i].text()
+                val cntFavorite = doc.select(".ItemRendererTextAvgScore")[i].text()
+                val cntRecom = doc.select(".ItemRendererTextAvgScore")[i].text()
+
+                db.bestDao().insert(
+                    DataBest(
+                        writerName,
+                        subject,
+                        bookImg,
+                        intro,
+                        bookCode,
+                        cntChapter,
+                        cntPageRead,
+                        cntFavorite,
+                        cntRecom,
+                        i + 1,
+                        Calendar.getInstance().get(Calendar.DAY_OF_WEEK),
+                        Calendar.getInstance().get(Calendar.DAY_OF_WEEK_IN_MONTH),
+                        Calendar.getInstance().get(Calendar.DATE),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        10 - i,
+                        "OneStore"
                     )
                 )
             }
