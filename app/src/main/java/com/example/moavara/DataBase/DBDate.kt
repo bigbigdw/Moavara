64cc788f6ca7bf4ecf195a25301a5f3cce6c308e
bigbigdw <kdw0310@ajou.ac.kr>
2022-02-08 08:45:15 +0900

    2022.02.08 주간 수정

--- app/src/main/java/com/example/moavara/DataBase/DBDate.kt
+++ app/src/main/java/com/example/moavara/DataBase/DBDate.kt
@@ -19,27 +19,36 @@ object DBDate {
 
         val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK).toString()
 
-        if (day == "1") {
-            return "sun"
-        } else if (day == "2") {
-            return "mon"
-        } else if (day == "3") {
-            return "tue"
-        } else if (day == "4") {
-            return "wed"
-        } else if (day == "5") {
-            return "thur"
-        } else if (day == "6") {
-            return "fri"
-        } else if (day == "7") {
-            return "sat"
-        } else {
-            return ""
+        when (day) {
+            "1" -> {
+                return "sun"
+            }
+            "2" -> {
+                return "mon"
+            }
+            "3" -> {
+                return "tue"
+            }
+            "4" -> {
+                return "wed"
+            }
+            "5" -> {
+                return "thur"
+            }
+            "6" -> {
+                return "fri"
+            }
+            "7" -> {
+                return "sat"
+            }
+            else -> {
+                return ""
+            }
         }
     }
 
     fun Week() : String {
-        return Calendar.getInstance().get(Calendar.DAY_OF_WEEK_IN_MONTH).toString()
+        return Calendar.getInstance().get(Calendar.WEEK_OF_MONTH).toString()
     }
 
     @SuppressLint("SimpleDateFormat")
