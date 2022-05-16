268744cf2cb2c3975142c6dd9ce4e5b120c15d8d
dwkim <kdw0310@ajou.ac.kr>
2022-05-16 19:47:20 +0900

    2022.05.16 로그인 구현

--- app/src/main/java/com/example/moavara/Main/ActivityGenre.kt
+++ app/src/main/java/com/example/moavara/Main/ActivityGenre.kt
@@ -1,70 +1,159 @@
 package com.example.moavara.Main
 
-import android.app.Notification
-import android.app.job.JobInfo
+import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
 import com.example.moavara.Firebase.FirebaseWorkManager
+import com.example.moavara.R
 import com.example.moavara.Search.WeekendDate
-import com.example.moavara.Util.BestRef
 import com.example.moavara.Util.DBDate
-import com.example.moavara.Util.Mining
 import com.example.moavara.databinding.ActivityGenreBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_genre.*
+import kotlinx.android.synthetic.main.activity_login.*
 import java.util.concurrent.TimeUnit
+
 
 class ActivityGenre : AppCompatActivity() {
 
     private lateinit var binding: ActivityGenreBinding
     var context = this
+    var mode = ""
+    var userInfo = mRootRef.child("User")
+    var genre = ""
+    var UID = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGenreBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mode = intent.getStringExtra("MODE") ?: "USER"
+        UID = intent.getStringExtra("UID") ?: ""
+
         with(binding){
+
+            if(mode == "USER" && UID != ""){
+                llayoutNickname.visibility = View.GONE
+                clayoutBtn.visibility = View.GONE
+                llayoutNickname.visibility = View.GONE
+                llayoutGenre.visibility = View.VISIBLE
+            } else {
+                llayoutNickname.visibility = View.VISIBLE
+                clayoutBtn.visibility = View.VISIBLE
+
+                btnNext.setOnClickListener{
+                    if(etviewNickname.text.toString() == ""){
+                        Toast.makeText(context, "닉네임을 설정해 주세요", Toast.LENGTH_SHORT).show()
+                    } else {
+                        llayoutNickname.visibility = View.GONE
+                        llayoutGenre.visibility = View.VISIBLE
+                        btnNext.visibility = View.GONE
+                        btnSubmit.visibility = View.VISIBLE
+                    }
+                }
+
+                btnSubmit.setOnClickListener {
+                    if(genre == ""){
+                        Toast.makeText(context, "장르를 선택해 주세요", Toast.LENGTH_SHORT).show()
+                    } else {
+                        userInfo.child(UID).child("Nickname").setValue(etviewNickname.text.toString())
+
+                        val myAlertBuilder: AlertDialog.Builder = AlertDialog.Builder(this@ActivityGenre)
+                        myAlertBuilder.setTitle("모아바라 가입")
+                        myAlertBuilder.setMessage("회원 정보를 확인해주세요 \n 닉네임 : " + etviewNickname.text + "\n선호장르 : $genre \n가 맞나요?")
+                        myAlertBuilder.setPositiveButton(
+                            "예"
+                        ) { _, _ ->
+                            val intent = Intent(context, ActivityMain::class.java)
+                            Toast.makeText(context, "모아바라에 오신것을 환영합니다", Toast.LENGTH_SHORT).show()
+                            startActivity(intent)
+                        }
+                        myAlertBuilder.setNegativeButton(
+                            "아니요"
+                        ) { _, _ ->
+                            finish()
+                        }
+                        // Alert를 생성해주고 보여주는 메소드(show를 선언해야 Alert가 생성됨)
+                        myAlertBuilder.show()
+                    }
+                }
+            }
+
             tviewMoavara.paintFlags = Paint.UNDERLINE_TEXT_FLAG
             tviewGenre.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
             llayoutBtn1.setOnClickListener {
-
                 savePreferences("FANTASY")
 
-                val novelIntent = Intent(context, ActivityMain::class.java)
-                startActivity(novelIntent)
+                if(mode == "USER"){
+                    val novelIntent = Intent(context, ActivityMain::class.java)
+                    startActivity(novelIntent)
+                } else {
+                    genre = "FANTASY"
+                    llayoutBtn1.setBackgroundResource(R.drawable.selector_genre_on)
+                    llayoutBtn2.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn3.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn4.setBackgroundResource(R.drawable.selector_genre)
+                    userInfo.child(UID).child("Genre").setValue("FANTASY")
+                }
+
             }
 
             llayoutBtn2.setOnClickListener {
-
-                savePreferences("ROMANCE")
-
-                val novelIntent = Intent(context, ActivityMain::class.java)
-                startActivity(novelIntent)
+                if(mode == "USER"){
+                    val novelIntent = Intent(context, ActivityMain::class.java)
+                    startActivity(novelIntent)
+                } else {
+                    genre = "ROMANCE"
+                    userInfo.child(UID).child("Genre").setValue("ROMANCE")
+                    llayoutBtn1.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn2.setBackgroundResource(R.drawable.selector_genre_on)
+                    llayoutBtn3.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn4.setBackgroundResource(R.drawable.selector_genre)
+                }
             }
 
             llayoutBtn3.setOnClickListener {
 
-                savePreferences("ALL")
-
-                val novelIntent = Intent(context, ActivityMain::class.java)
-                startActivity(novelIntent)
+                if(mode == "USER"){
+                    val novelIntent = Intent(context, ActivityMain::class.java)
+                    startActivity(novelIntent)
+                } else {
+                    genre = "ALL"
+                    userInfo.child(UID).child("Genre").setValue("ALL")
+                    llayoutBtn1.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn2.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn3.setBackgroundResource(R.drawable.selector_genre_on)
+                    llayoutBtn4.setBackgroundResource(R.drawable.selector_genre)
+                }
             }
 
             llayoutBtn4.setOnClickListener {
 
-                savePreferences("BL")
-
-                val novelIntent = Intent(context, ActivityMain::class.java)
-                startActivity(novelIntent)
+                if(mode == "USER"){
+                    val novelIntent = Intent(context, ActivityMain::class.java)
+                    startActivity(novelIntent)
+                } else {
+                    genre = "BL"
+                    userInfo.child(UID).child("Genre").setValue("BL")
+                    llayoutBtn1.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn2.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn3.setBackgroundResource(R.drawable.selector_genre)
+                    llayoutBtn4.setBackgroundResource(R.drawable.selector_genre_on)
+                }
             }
         }
 
@@ -115,6 +204,46 @@ class ActivityGenre : AppCompatActivity() {
 
         }.addOnFailureListener{}
 
+    }
+
+
+    override fun onBackPressed() {
+        if(mode != "USER") {
+            if(llayoutNickname.visibility == View.VISIBLE){
+                val myAlertBuilder: AlertDialog.Builder = AlertDialog.Builder(this@ActivityGenre)
+                myAlertBuilder.setTitle("모아바라 가입")
+                myAlertBuilder.setMessage("가입을 그만두고 로그인 화면으로 돌아가시겠습니까?")
+                myAlertBuilder.setPositiveButton(
+                    "예"
+                ) { _, _ ->
+
+                    val currentUser = FirebaseAuth.getInstance().currentUser
+                    currentUser?.delete()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                userInfo.child(UID).removeValue()
+                                super.onBackPressed()
+                                finish()
+                            }
+                        }
+
+                }
+                myAlertBuilder.setNegativeButton(
+                    "아니요"
+                ) { _, _ ->
+                    finish()
+                }
+                // Alert를 생성해주고 보여주는 메소드(show를 선언해야 Alert가 생성됨)
+                myAlertBuilder.show()
+
+            } else if(llayoutGenre.visibility == View.VISIBLE){
+                llayoutNickname.visibility = View.VISIBLE
+                llayoutGenre.visibility = View.GONE
+            }
+
+        } else {
+            super.onBackPressed()
+        }
     }
 
     private fun savePreferences(genre: String) {
--- app/src/main/java/com/example/moavara/Main/ActivityLogin.kt
+++ app/src/main/java/com/example/moavara/Main/ActivityLogin.kt
@@ -2,65 +2,57 @@ package com.example.moavara.Main
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moavara.R
-import com.example.moavara.Search.BookListDataBestToday
-import com.example.moavara.Search.CommunityBoard
-import com.example.moavara.Util.DBDate
-import com.example.moavara.Util.Mining
-import com.example.moavara.databinding.ActivityBestDetailBinding
 import com.example.moavara.databinding.ActivityLoginBinding
-import com.facebook.AccessToken
 import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_login.*
-import java.util.*
+import kotlin.system.exitProcess
 
 class ActivityLogin : AppCompatActivity() {
-    private var auth : FirebaseAuth? = null
-    private var googleSignInClient : GoogleSignInClient? = null
+    private var auth: FirebaseAuth? = null
+    private var googleSignInClient: GoogleSignInClient? = null
     private var GOOGLE_LOGIN_CODE = 9001
-    private var callbackManager : CallbackManager? = null
+    private var callbackManager: CallbackManager? = null
     private lateinit var binding: ActivityLoginBinding
     var context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
         callbackManager = CallbackManager.Factory.create()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding){
+        with(binding) {
 
             // 구글 로그인 버튼
             btnLogin.setOnClickListener { googleLogin() }
 
             // 로그인 버튼
             btnRegister.setOnClickListener {
-                auth?.signOut()
-                googleSignInClient?.signOut()
-                Toast.makeText(context,"로그아웃 성공",Toast.LENGTH_SHORT).show()
+                val currentUser = FirebaseAuth.getInstance().currentUser
+                currentUser?.delete()
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            auth?.signOut()
+                            googleSignInClient?.signOut()
+                            Toast.makeText(context, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+                        }
+                    }
             }
         }
 
@@ -69,7 +61,7 @@ class ActivityLogin : AppCompatActivity() {
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     // 로그아웃하지 않을 시 자동 로그인 , 회원가입시 바로 로그인 됨
@@ -79,44 +71,46 @@ class ActivityLogin : AppCompatActivity() {
     }
 
     // 구글 로그인 함수
-    fun googleLogin(){
+    fun googleLogin() {
         var signInIntent = googleSignInClient?.signInIntent
-        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
-    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
-        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)
-            ?.addOnCompleteListener{
-                    task ->
+            ?.addOnCompleteListener { task ->
 
-                Log.d("@@@", task.result.toString());
-
-                if(task.isSuccessful){
+                if (task.isSuccessful) {
                     // 아이디, 비밀번호 맞을 때
 //                    moveMainPage(task.result?.user)
 
-                    mRootRef.child("User").child(task.result?.user?.uid.toString()).addValueEventListener(object : ValueEventListener {
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            if(dataSnapshot.exists()){
-                                Toast.makeText(context,"환영한다 기존회원",Toast.LENGTH_SHORT).show()
-                                finish()
-                            } else {
-                                Toast.makeText(context,"환영한다 뉴비",Toast.LENGTH_SHORT).show()
-                                mRootRef.child("User").child(task.result?.user?.uid.toString()).setValue("HIHI")
+                    mRootRef.child("User").child(task.result?.user?.uid.toString())
+                        .addListenerForSingleValueEvent(object :
+                            ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    Toast.makeText(context, "환영한다 기존회원", Toast.LENGTH_SHORT).show()
+                                    finish()
+                                } else {
+                                    Toast.makeText(context, "환영한다 뉴비", Toast.LENGTH_SHORT).show()
+                                    mRootRef.child("User").child(task.result?.user?.uid.toString())
+                                        .setValue("HIHI")
 
-                                startActivity(Intent(context,ActivityRegister::class.java))
-                                finish()
+                                    val intent = Intent(context, ActivityGenre::class.java)
+                                    intent.putExtra("MODE", "NEWBIE")
+                                    intent.putExtra("UID", task.result?.user?.uid.toString())
+                                    startActivity(intent)
+                                    finish()
+                                }
                             }
-                        }
 
-                        override fun onCancelled(databaseError: DatabaseError) {}
-                    })
-
-
-                }else{
+                            override fun onCancelled(databaseError: DatabaseError) {}
+                        })
+                } else {
                     // 틀렸을 때
-                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                    Log.d("####", task.exception?.message.toString())
                 }
             }
     }
@@ -124,19 +118,18 @@ class ActivityLogin : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        callbackManager?.onActivityResult(requestCode,resultCode,data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == GOOGLE_LOGIN_CODE){
+        if (requestCode == GOOGLE_LOGIN_CODE) {
             var result = data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }!!
             // 구글API가 넘겨주는 값 받아옴
 
-            if(result.isSuccess) {
+            if (result.isSuccess) {
                 var accout = result.signInAccount
                 firebaseAuthWithGoogle(accout)
-                Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
-            }
-            else{
-                Toast.makeText(this,"로그인 실패",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -144,9 +137,9 @@ class ActivityLogin : AppCompatActivity() {
 
 
     // 유저정보 넘겨주고 메인 액티비티 호출
-    fun moveMainPage(user: FirebaseUser?){
-        if(user!= null){
-            startActivity(Intent(this,ActivityMain::class.java))
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, ActivityMain::class.java))
             finish()
         }
     }
