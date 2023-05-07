7936336e5e7df99e64978ac3299d7f0d4f2efcab
bigbigdw <bigbigdw0310@gmail.com>
2023-05-07 23:43:23 +0900

    2023.05.07 HILT 적용

--- app/src/main/java/com/example/moavara/App.kt
+++ app/src/main/java/com/example/moavara/App.kt
@@ -1,7 +1,7 @@
 package com.example.moavara
 
 import android.app.Application
-//import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.HiltAndroidApp
 
-//@HiltAndroidApp
+@HiltAndroidApp
 class App : Application()
